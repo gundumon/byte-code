96c8ff2e13cd4520ef9c57c265696239f7582408
Raju Joseph <raju.joseph@live.com>
2025-09-18 16:57:18 +0530

    ...

--- csharp/coding-patterns/palindrome.cs
+++ csharp/coding-patterns/palindrome.cs
@@ -1,8 +1,24 @@
-var input = args.Length > 0 ? args[0] ?? string.Empty : string.Empty;
+bool IsPalindrome(string[] args) {
+    var input = args.Length > 0 ? args[0] ?? string.Empty : string.Empty;
 
-var left = 0;
-var right = input.Length - 1;
+    var left = 0;
+    var right = input.Length - 1;
 
-while (left < right) {
+    while (left < right) {
+        while ((left < right) && !char.IsLetterOrDigit(input[left])) {
+            left += 1;
+        }
 
+        while ((left < right) && !char.IsLetterOrDigit(input[right])) {
+            right -= 1;
+        }
+
+        if (input[left] != input[right])
+            return false;
+
+        left += 1;
+        right -= 1;
+    }
 }
+
+IsPalindrome(args);
