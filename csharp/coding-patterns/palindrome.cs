36c8b1a7b120cfb031f42d7f2add4ffa99976853
Raju Joseph <raju.joseph@live.com>
2025-09-18 17:03:23 +0530

    ...

--- csharp/coding-patterns/palindrome.cs
+++ csharp/coding-patterns/palindrome.cs
@@ -1,24 +1,35 @@
-bool IsPalindrome(string[] args) {
-    var input = args.Length > 0 ? args[0] ?? string.Empty : string.Empty;
-
+bool IsPalindrome(string input) {   
     var left = 0;
     var right = input.Length - 1;
 
     while (left < right) {
+        // skip non-alphanumeric characters from the left
         while ((left < right) && !char.IsLetterOrDigit(input[left])) {
             left += 1;
         }
 
+        // skip non-alphanumeric characters from the right
         while ((left < right) && !char.IsLetterOrDigit(input[right])) {
             right -= 1;
         }
 
+        // if the characters at the left and right pointers do not match, the string is not a palindrome
         if (input[left] != input[right])
             return false;
 
         left += 1;
         right -= 1;
     }
+
+    return true;
 }
 
-IsPalindrome(args);
+var input = args.Length > 0 ? args[0] ?? string.Empty : string.Empty;
+var result = IsPalindrome(input);
+
+if (result) {
+    Console.WriteLine($"{input} is a palindrome.");
+}
+else {
+    Console.WriteLine($"{input} is not a palindrome.");
+}
