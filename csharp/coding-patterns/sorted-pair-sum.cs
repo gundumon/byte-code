7df6d7c22e00e8f06125466257ff25d49b600ddb
Raju Joseph <raju.joseph@live.com>
2025-09-19 21:28:45 +0530

    ...

--- csharp/coding-patterns/sorted-pair-sum.cs
+++ csharp/coding-patterns/sorted-pair-sum.cs
@@ -2,7 +2,7 @@
 Given an array of integers sorted in ascending order and a target value, return the indexes of any pair of numbers in the array that sum to the target. The order of the indexes in the result doesn't matter. If no pair is found, return an empty array.
 */
 
-ValueTuple<int, int>? IndexesThatSumToTarget(int[] nums, int target) {
+List<int> IndexesThatSumToTarget(int[] nums, int target) {
     var left = 0;
     var right = nums.Length - 1;
 
@@ -22,12 +22,8 @@ ValueTuple<int, int>? IndexesThatSumToTarget(int[] nums, int target) {
     return [];
 }
 
-var nums1 = [-5, -2, 3, 4, 6];
-var tgt1 = 7;
-var indexes = IndexesThatSumToTarget(nums1, tgt1);
-Console.WriteLine($"{nums1} | {tgt1} -> {indexes}");
+int[] nums = [-5, -2, 3, 4, 6];
+int target = 7;
 
-nums1 = [1, 1, 1];
-tgt1 = 2;
-indexes = IndexesThatSumToTarget(nums1, tgt1);
-Console.WriteLine($"{nums1} | {tgt1} -> {indexes}");
+var result = IndexesThatSumToTarget(nums, target);
+Console.WriteLine($"[{string.Join(", ", nums)}], {target} -> [{string.Join(", ", result)}]");
