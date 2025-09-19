5554ceeee3f07b19aae5c5852f0ad09d64b61152
Raju Joseph <raju.joseph@live.com>
2025-09-19 20:21:54 +0530

    ...

--- csharp/coding-patterns/sorted-pair-sum.cs
+++ csharp/coding-patterns/sorted-pair-sum.cs
@@ -2,4 +2,7 @@
 Given an array of integers sorted in ascending order and a target value, return the indexes of any pair of numbers in the array that sum to the target. The order of the indexes in the result doesn't matter. If no pair is found, return an empty array.
 */
 
-(int index1, int index2)
+(int index1, int index2) IndexesThatSumToTarget(int[] nums, int target) {
+    var left = 0;
+    var right = nums.Length - 1
+}
