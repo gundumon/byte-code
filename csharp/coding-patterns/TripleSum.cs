0930b8c1b5cf37526382e3d0133390ff4467ba68
Raju Joseph <raju.joseph@live.com>
2025-09-20 21:41:32 +0530

    ...

---
+++ csharp/coding-patterns/TripleSum.cs
@@ -0,0 +1,14 @@
+﻿namespace CodingPatterns;
+
+/*
+Given an array of integers, return all triplets [a, b, c] such that a + b + c = 0 . The solution must not contain duplicate triplets (e.g., [1, 2, 3] and [2, 3, 1] are considered duplicates). If no such triplets are found, return an empty array.
+
+Example:
+    Input: [0, -1, 2, -3, 1]
+    Output: [[-3, 1, 2], [-1, 0, 1]]
+*/
+public class TripleSum {
+    List<Tuple<int, int, int>> FindAllTriplete(int[] nums) {
+        return [];
+    }
+}
