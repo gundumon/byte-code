597142f83850bc701ab60db4b1b1b8c1aaa4d2b2
Raju Joseph <raju.joseph@live.com>
2025-09-20 20:58:46 +0530

    ...

--- csharp/coding-patterns/SortedPairSum.cs
+++ csharp/coding-patterns/SortedPairSum.cs
@@ -1,3 +1,5 @@
+namespace CodingPatterns;
+
 /*
 Given an array of integers sorted in ascending order and a target value, return the indexes of any pair of numbers in the array that sum to the target. The order of the indexes in the result doesn't matter. If no pair is found, return an empty array.
 
@@ -6,23 +8,24 @@ Example:
     int target = 7
     output: [2, 3]
 */
+public class SortedPairSum {
+    public List<int> IndexesThatSumToTarget(int[] nums, int target) {
+        var left = 0;
+        var right = nums.Length - 1;
 
-List<int> IndexesThatSumToTarget(int[] nums, int target) {
-    var left = 0;
-    var right = nums.Length - 1;
+        while (left < right) {
+            var sum = nums[left] + nums[right];
 
-    while (left < right) {
-        var sum = nums[left] + nums[right];
+            // if the sum is smaller, increment the left pointer to increase the sum
+            if (sum < target)
+                left += 1;
+            // if the sum is larger, decrement the right pointer to decrease the sum
+            else if (sum > target)
+                right -= 1;
+            else
+                return [left, right];
+        }
 
-        // if the sum is smaller, increment the left pointer to increase the sum
-        if (sum < target)
-            left += 1;
-        // if the sum is larger, decrement the right pointer to decrease the sum
-        else if (sum > target)
-            right -= 1;
-        else
-            return [left, right];
+        return [];
     }
-
-    return [];
 }
