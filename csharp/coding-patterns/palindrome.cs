f6dd323b1719fca9392f6178f5f28a25a20c8eb8
Raju Joseph <raju.joseph@live.com>
2025-09-21 18:55:50 +0530

    ...

--- csharp/coding-patterns/palindrome.cs
+++ csharp/coding-patterns/palindrome.cs
@@ -1,30 +1,38 @@
-//bool IsPalindrome(string input) {
-//    var left = 0;
-//    var right = input.Length - 1;
+namespace CodingPatterns;
 
-//    while (left < right) {
-//        // skip non-alphanumeric characters from the left
-//        while ((left < right) && !char.IsLetterOrDigit(input[left])) {
-//            left += 1;
-//        }
+/*
+Given a string, determine if it's a palindrome after removing all non-alphanumeric characters. A character is alphanumeric if it's either a letter or a number.
+*/
+public class Palindrom {
 
-//        // skip non-alphanumeric characters from the right
-//        while ((left < right) && !char.IsLetterOrDigit(input[right])) {
-//            right -= 1;
-//        }
+    //bool IsPalindrome(string input) {
+    //    var left = 0;
+    //    var right = input.Length - 1;
 
-//        // if the characters at the left and right pointers do not match, the string is not a palindrome
-//        if (input[left] != input[right])
-//            return false;
+    //    while (left < right) {
+    //        // skip non-alphanumeric characters from the left
+    //        while ((left < right) && !char.IsLetterOrDigit(input[left])) {
+    //            left += 1;
+    //        }
 
-//        left += 1;
-//        right -= 1;
-//    }
+    //        // skip non-alphanumeric characters from the right
+    //        while ((left < right) && !char.IsLetterOrDigit(input[right])) {
+    //            right -= 1;
+    //        }
 
-//    return true;
-//}
+    //        // if the characters at the left and right pointers do not match, the string is not a palindrome
+    //        if (input[left] != input[right])
+    //            return false;
 
-//var input = args.Length > 0 ? args[0] ?? string.Empty : string.Empty;
-//var result = IsPalindrome(input);
+    //        left += 1;
+    //        right -= 1;
+    //    }
 
-//Console.WriteLine($"{input} is {(result ? "a palindrome." : "not a palindrome.")}");
+    //    return true;
+    //}
+
+    //var input = args.Length > 0 ? args[0] ?? string.Empty : string.Empty;
+    //var result = IsPalindrome(input);
+
+    //Console.WriteLine($"{input} is {(result ? "a palindrome." : "not a palindrome.")}");
+}
