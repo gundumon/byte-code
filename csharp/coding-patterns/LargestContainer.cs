0ef25e9f8dac90bf264bed19a3e9495aeb371102
Raju Joseph <raju.joseph@live.com>
2025-09-22 20:51:10 +0530

    ...

--- csharp/coding-patterns/LargestContainer.cs
+++ csharp/coding-patterns/LargestContainer.cs
@@ -4,7 +4,7 @@
 You are given an array of numbers, each representing the height of a vertical line on a graph. A container can be formed with any pair of these lines, along with the x-axis of the graph. Return the amount of water which the largest container can hold.
 */
 public class LargestContainer {
-    public static int FindLargest(int[] heights) {
+    public static int Find(int[] heights) {
         int max_water = 0;
         int left = 0;
         int right = heights.Length - 1;
