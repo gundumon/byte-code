c8dbf9ab00042b7f08f05ee300f4d4dcb3f7d1c7
Raju Joseph <raju.joseph@live.com>
2025-09-22 19:04:32 +0530

    ...

--- csharp/coding-patterns/LargestContainer.cs
+++ csharp/coding-patterns/LargestContainer.cs
@@ -1,4 +1,7 @@
 ﻿namespace CodingPatterns;
 
+/*
+You are given an array of numbers, each representing the height of a vertical line on a graph. A container can be formed with any pair of these lines, along with the x-axis of the graph. Return the amount of water which the largest container can hold.
+*/
 public static class LargestContainer {
 }
