cac28bde4741a69c95528874545368cfae6759bd
Raju Joseph <raju.joseph@live.com>
2025-09-23 18:53:50 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -3,5 +3,7 @@
 List<int[]> nums = [[0, 1, 0, 3, 2], [], [0], [1], [0, 0, 0], [1, 3, 2], [1, 1, 1, 0, 0], [0, 0, 1, 1, 1]];
 foreach(var num in nums) {
     var result = ShiftZero.Shift(num);
-    Console.WriteLine($"");
+    Console.WriteLine($"[{string.Join(", ", num)}] -> [{string.Join(", ", result)}]");
 }
+
+Console.ReadLine();
