d581f564f1b25ab39488fc7614990e8349fcbf0b
Raju Joseph <raju.joseph@live.com>
2025-09-23 18:49:04 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using CodingPatterns;
+
+int[] nums = [0, 0, 1, 1, 1];
+var result = ShiftZero.Shift(nums);
+Console.WriteLine(result);
+Console.ReadLine();
--- csharp/coding-patterns/ShiftZero.cs
+++ csharp/coding-patterns/ShiftZero.cs
@@ -6,13 +6,11 @@ public class ShiftZero {
         int right = 0;
 
         while (right < nums.Length) {
-            if (nums[right] == 0) {
-                right += 1;
-                continue;
+            if ((nums[right] != 0) && (right != left)) {
+                (nums[left], nums[right]) = (nums[right], nums[left]);
+                left += 1;
             }
-
-            (nums[left], nums[right]) = (nums[right], nums[left]);
-            left += 1;
+            
             right += 1;
         }
 
