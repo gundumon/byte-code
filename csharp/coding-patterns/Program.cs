8818cbb55f257e4f673b1f90f975b2eb9d65d2b0
Raju Joseph <raju.joseph@live.com>
2025-09-23 19:10:31 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -1,9 +1,1 @@
-﻿using CodingPatterns;
-
-List<int[]> nums = [[0, 1, 0, 3, 2], [], [0], [1], [0, 0, 0], [1, 3, 2], [1, 1, 1, 0, 0], [0, 0, 1, 1, 1]];
-foreach(var num in nums) {
-    var result = ShiftZero.Shift(num);
-    Console.WriteLine($"[{string.Join(", ", num)}] -> [{string.Join(", ", result)}]");
-}
-
-Console.ReadLine();
+﻿
