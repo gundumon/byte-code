293bced7805d515aef40fa7ddcd039ea51e86f2d
Raju Joseph <raju.joseph@live.com>
2025-09-23 18:36:48 +0530

    ...

--- csharp/coding-patterns/ShiftZero.cs
+++ csharp/coding-patterns/ShiftZero.cs
@@ -1,4 +1,21 @@
 ﻿namespace CodingPatterns;
 
 public class ShiftZero {
+    public static int[] Shift(int[] nums) {
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.Length) {
+            if (nums[right] == 0) {
+                right += 1;
+                continue;
+            }
+
+            (nums[left], nums[right]) = (nums[right], nums[left]);
+            left += 1;
+            right += 1;
+        }
+
+        return nums;
+    }
 }
