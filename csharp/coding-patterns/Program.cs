c62e80120c9acc9104d6a8294640e253501d59a6
Raju Joseph <raju.joseph@live.com>
2025-09-23 18:52:25 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CodingPatterns;
 
-int[] nums = [0, 0, 1, 1, 1];
-var result = ShiftZero.Shift(nums);
-Console.WriteLine(result);
-Console.ReadLine();
+List<int[]> nums = [[0, 1, 0, 3, 2], [], [0], [1], [0, 0, 0], [1, 3, 2], [1, 1, 1, 0, 0], [0, 0, 1, 1, 1]];
+foreach(var num in nums) {
+    var result = ShiftZero.Shift(num);
+    Console.WriteLine($"");
+}
