7f1b6307d3909e79d2a94b324084fedc03719841
Raju Joseph <raju.joseph@live.com>
2025-09-25 19:45:53 +0530

    ...

--- csharp/coding-patterns/LexicographicalSequence.cs
+++ csharp/coding-patterns/LexicographicalSequence.cs
@@ -2,7 +2,7 @@
 
 public class LexicographicalSequence {
     /*
-
+    Given a string of lowercase English letters, rearrange the characters to form a new string representing the next immediate sequence in lexicographical (alphabetical) order. If the given string is already last in lexicographical order among all possible arrangements, return the arrangement that's first in lexicographical order.
     */
     public static string NextSequence(string input) {
 
