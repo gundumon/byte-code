71d32271fed36b97955338bc12158e6f45f2665e
Raju Joseph <raju.joseph@live.com>
2025-09-27 18:55:06 +0530

    ...

--- csharp/coding-patterns/UnsortedPairSum.cs
+++ csharp/coding-patterns/UnsortedPairSum.cs
@@ -1,4 +1,11 @@
 ﻿namespace CodingPatterns;
 
+/*
+Given an array of integers, return the indexes of any two numbers that add up to a target. The order of the indexes in the result doesn't matter. If no pair is found, return an empty array.
+
+Example:
+    Input: nums = [-1, 3, 4, 2], target = 3
+    Output: [0, 2]
+*/
 public class UnsortedPairSum {
 }
