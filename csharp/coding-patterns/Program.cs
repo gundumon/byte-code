27175b67164613a3b03a8bf1fd885bf3d68b41bb
Raju Joseph <raju.joseph@live.com>
2025-09-27 21:28:03 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -1,9 +1,1 @@
 ﻿
-using CodingPatterns;
-
-int[] nums = [-1, 3, 4, 2];
-int target = 3;
-
-var result = UnsortedPairSum.FindPairs(nums, target);
-Console.WriteLine($"[{string.Join(", ", nums)}] : {target} => [{string.Join(", ", result)}]");
-Console.ReadLine();
