d7e4c5431b6191bc6ff771dffa5782ce1106bd0c
Raju Joseph <raju.joseph@live.com>
2025-09-27 19:46:45 +0530

    ...

--- csharp/coding-patterns/UnsortedPairSum.cs
+++ csharp/coding-patterns/UnsortedPairSum.cs
@@ -3,12 +3,23 @@
 /*
 Given an array of integers, return the indexes of any two numbers that add up to a target. The order of the indexes in the result doesn't matter. If no pair is found, return an empty array.
 
+Constraint: The same index cannot be used twice in the result
+
 Example:
     Input: nums = [-1, 3, 4, 2], target = 3
     Output: [0, 2]
 */
 public class UnsortedPairSum {
     public static int[] FindPairs(int[] nums, int target) {
+        Dictionary<int, int> hashmap = [];
+
+        for(int i = 0; i < nums.Length; i++) {
+            int x = nums[i];
+            if (hashmap.ContainsKey(target - x)) {
+                return [i, hashmap[target - x]];
+            }
+        }
+
         return [];
     }
 }
