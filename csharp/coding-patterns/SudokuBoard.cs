abebe83e7e75f41259230d865e0de0e3d6859e9b
Raju Joseph <raju.joseph@live.com>
2025-09-28 19:39:55 +0530

    ...

--- csharp/coding-patterns/SudokuBoard.cs
+++ csharp/coding-patterns/SudokuBoard.cs
@@ -4,6 +4,9 @@
 Given a partially completed 9×9 Sudoku board, determine if the current state of the board adheres to the rules of the game:
     - Each row and column must contain unique numbers between 1 and 9, or be empty (represented as 0)
     - Each of the nine 3×3 subgrids that compose the grid must contain unique numbers between 1 and 9, or be empty
+
+Constraings:
+    - Assume each integer on the board falls in the range of [0, 9]
 */
 public class SudokuBoard {
     public static void Verify() {
