4e680261745f28307acaa591a0922de3a40359ea
Raju Joseph <raju.joseph@live.com>
2025-09-29 19:35:02 +0530

    ...

--- csharp/coding-patterns/ZeroStripping.cs
+++ csharp/coding-patterns/ZeroStripping.cs
@@ -4,11 +4,13 @@
 For each zero in an m x n matrix, set its entire row and column to zero in place.
 */
 public class ZeroStripping {
-    public static void SetZeroes(List<List<int>> matrix) {
+    public static List<List<int>> SetZeroes(List<List<int>> matrix) {
         HashSet<int> zero_rows = [];
         HashSet<int> zero_columns = [];
 
-        for(int r = 0; r < matrix[0].Count; r++) {
+        // pass 1: traverse through the matrix to identify the rows and columns
+        // containing zeros and store their indexes in the appropriate hash sets
+        for (int r = 0; r < matrix[0].Count; r++) {
             for (int c = 0; c < matrix.Count; c++) {
                 if (matrix[r][c] == 0) {
                     zero_rows.Add(r);
@@ -17,10 +19,16 @@ public class ZeroStripping {
             }
         }
 
+        // pass 2: set any cell in the matrix to zero if its row index is in 'zero_rows' 
+        // or its column index is in 'zero_columns'
         for (int r = 0; r < matrix[0].Count; r++) {
             for (int c = 0; c < matrix.Count; c++) {
-                
+                if (zero_rows.Contains(r) || zero_columns.Contains(c)) {
+                    matrix[r][c] = 0;
+                }
             }
         }
+
+        return matrix;
     }
 }
