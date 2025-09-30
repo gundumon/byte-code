8a6a62277de4a70a18204b4c246703a6dbb37acb
Raju Joseph <raju.joseph@live.com>
2025-09-30 19:12:35 +0530

    ...

--- csharp/coding-patterns/LongestChain.cs
+++ csharp/coding-patterns/LongestChain.cs
@@ -1,4 +1,11 @@
 ﻿namespace CodingPatterns;
 
-internal class LongestChain {
+/*
+Find the longest chain of consecutive numbers in an array. Two numbers are consecutive if they have a difference of 1
+
+Example:
+    Input: [1, 6, 2, 5, 8, 7, 10, 3]
+    Output: 4 (The longest chain of consecutive numbers is 5, 6, 7, 8)
+*/
+public class LongestChain {
 }
