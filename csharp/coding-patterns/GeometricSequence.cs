a4db43011c14b9456b72bfaf6e74e8b3e80f4a9c
Raju Joseph <raju.joseph@live.com>
2025-10-01 12:19:15 +0530

    ...

--- csharp/coding-patterns/GeometricSequence.cs
+++ csharp/coding-patterns/GeometricSequence.cs
@@ -9,13 +9,26 @@ Example:
     Output: 5
 */
 public class GeometricSequence {
-    public static int FindTriplets(List<int> nums, int commonRation) {
+    public static int FindTriplets(List<int> nums, int ratio) {
         Dictionary<int, int> leftDict = [];
         Dictionary<int, int> rightDict = [];
         int count = 0;
 
+        // populate the right dictionary with the frequency of each element in the array
         foreach(var num in nums) {
             rightDict.Add(num, rightDict.GetValueOrDefault(num, 0) + 1);
+        }
+
+        // search for geometric triplets that have x as the center
+        foreach(var num in nums) {
+            // decrement the frequency of num in right dictionary since num is now being processed and is no loger to the right
+            rightDict[num] = rightDict[num] - 1;
+
+            if (num % ratio == 0) {
+                count += leftDict.GetValueOrDefault(num / ratio, 0) * rightDict.GetValueOrDefault(num * ratio, 0);
+            }
+
+            leftDict.Add(num, leftDict.GetValueOrDefault(num, 0) + 1);
         }
     }
 }
