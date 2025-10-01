3bfed1a91f86a9052c229f91eacece026f60470a
Raju Joseph <raju.joseph@live.com>
2025-10-01 09:51:39 +0530

    ...

--- csharp/coding-patterns/GeometricSequence.cs
+++ csharp/coding-patterns/GeometricSequence.cs
@@ -1,7 +1,15 @@
 ﻿namespace CodingPatterns;
 
+/*
+Given an array of integers and a common ratio r, find all triplets of indexes (i, j, k) that follow a geometric sequence for i < j < k. It's possible to encounter duplicate triplets in the array.
+
+Example:
+    Input: [2, 1, 2, 4, 8, 8]
+    Ration: 2
+    Output: 5
+*/
 public class GeometricSequence {
-    public static List<int> FindTriplets(List<int> nums, int commonRation) {
-        return [];
+    public static int FindTriplets(List<int> nums, int commonRation) {
+        return 0;
     }
 }
