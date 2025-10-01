fbcc9adb01e7b0bc50a289d159bf927acac56585
Raju Joseph <raju.joseph@live.com>
2025-10-01 09:06:22 +0530

    ...

--- csharp/coding-patterns/GeometricSequence.cs
+++ csharp/coding-patterns/GeometricSequence.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace CodingPatterns;
 
-namespace CodingPatterns {
-    internal class GeometricSequence {
-    }
+public class GeometricSequence {
 }
