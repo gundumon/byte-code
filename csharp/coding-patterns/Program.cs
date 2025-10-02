2f6a5c35ebddc38fb1dd2ee965843b4e195614f4
Raju Joseph <raju.joseph@live.com>
2025-10-02 19:09:14 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -1,8 +1,1 @@
-﻿using CodingPatterns;
-
-List<int> nums = [2, 1, 2, 4, 8, 8];
-int ratio = 2;
-
-var count = GeometricSequence.FindTriplets(nums, ratio);
-Console.WriteLine(count);
-Console.ReadLine();
+﻿
