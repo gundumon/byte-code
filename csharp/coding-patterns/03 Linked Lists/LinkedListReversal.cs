59cb95ce0a91d57f231ffee117a0a292983fd68c
Raju Joseph <raju.joseph@live.com>
2025-10-02 19:45:45 +0530

    ...

--- csharp/coding-patterns/03 Linked Lists/LinkedListReversal.cs
+++ csharp/coding-patterns/03 Linked Lists/LinkedListReversal.cs
@@ -5,6 +5,15 @@ Reverse a singly linked list
 */
 public class LinkedListReversal {
     public static LinkedListNode<int> Reverse(LinkedListNode<int> head) {
+        LinkedListNode<int> currNode = head;
+        LinkedListNode<int> prevNode = null;
 
+        while (currNode != null) {
+            var nextNode = currNode.Next;
+            currNode.Next = prevNode;
+            
+            prevNode = currNode;
+            currNode = nextNode;
+        }
     }
 }
