6d33059898f595fddd864ec278c5c60493f2691a
Raju Joseph <raju.joseph@live.com>
2025-10-03 11:48:39 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -1,15 +1,1 @@
-﻿using CodingPatterns;
-
-ListNode<int> a = new ListNode<int>(1);
-ListNode<int> b = new ListNode<int>(2);
-ListNode<int> c = new ListNode<int>(4);
-ListNode<int> d = new ListNode<int>(7);
-ListNode<int> e = new ListNode<int>(3);
-
-a.Next = b;
-b.Next = c;
-c.Next = d;
-d.Next = e;
-
-var result = LinkedListReversal.Reverse(a);
-Console.ReadLine();
+﻿
