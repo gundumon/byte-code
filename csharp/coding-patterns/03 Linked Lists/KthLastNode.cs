bd4c3013a0ebac64f0b2e5d8c90f3611e09b6ea4
Raju Joseph <raju.joseph@live.com>
2025-10-05 18:32:55 +0530

    ...

--- csharp/coding-patterns/03 Linked Lists/KthLastNode.cs
+++ csharp/coding-patterns/03 Linked Lists/KthLastNode.cs
@@ -1,6 +1,12 @@
 ﻿namespace CodingPatterns;
 
 public class KthLastNode {
+    /*
+    Return the head of a singly linked list after removing the kth node from the end of it
+
+    Constraint:
+        The linked list contains at least one node
+    */
     public static void Remove() {
 
     }
