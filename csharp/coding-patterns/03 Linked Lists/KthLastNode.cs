a361e468b65e011bbe20f1b1813fdafff05bb29f
Raju Joseph <raju.joseph@live.com>
2025-10-05 19:39:52 +0530

    ...

--- csharp/coding-patterns/03 Linked Lists/KthLastNode.cs
+++ csharp/coding-patterns/03 Linked Lists/KthLastNode.cs
@@ -8,6 +8,13 @@ Constraint:
 */
 public class KthLastNode {
     public ListNode<int> Remove(ListNode<int> head, int k) {
+        ListNode<int> dummy = new(-1);
+        dummy.Next = head;
+        ListNode<int> trainer = dummy;
+        ListNode<int> leader = dummy;
 
+        for (var i = 0; i < k; i++) {
+
+        }
     }
 }
