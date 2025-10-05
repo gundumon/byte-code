c5bd9e48e6910bd7058186a711314ce410c38ae4
Raju Joseph <raju.joseph@live.com>
2025-10-05 19:45:42 +0530

    ...

--- csharp/coding-patterns/03 Linked Lists/KthLastNode.cs
+++ csharp/coding-patterns/03 Linked Lists/KthLastNode.cs
@@ -11,8 +11,8 @@ public class KthLastNode {
         // a dummy node to ensure there is a node before 'head' in case we need to remove the head node
         ListNode<int> dummy = new(-1);
         dummy.Next = head;
-        ListNode<int> trainer = dummy;
-        ListNode<int> leader = dummy;
+        ListNode<int> trailer = dummy;
+        ListNode<int>? leader = dummy;
 
         // advance leader 'k' steps ahead
         for (var i = 0; i < k; i++) {
@@ -22,5 +22,14 @@ public class KthLastNode {
                 return head;
             }
         }
+
+        // move 'leader' to the end of the linked list, keeping 'trailer' k nodes behind
+        while (leader != null) {
+            leader = leader.Next;
+            trailer = trailer.Next;
+        }
+
+        // remove the kth node from the end
+
     }
 }
