48baae656512fed3f8afc158fab8e40b037fe389
Raju Joseph <raju.joseph@live.com>
2025-10-06 21:51:18 +0530

    ...

--- csharp/coding-patterns/03 Linked Lists/LinkedListIntersection.cs
+++ csharp/coding-patterns/03 Linked Lists/LinkedListIntersection.cs
@@ -4,7 +4,15 @@
 Return the node where two singly linked lists intersect. If the linked lists don't intersect, return null.
 */
 public class LinkedListIntersection {
-    public static void FindNode() {
-        //
+    public static ListNode<int>? FindNode(ListNode<int> headA, ListNode<int> headB) {
+        var ptrA = headA;
+        var ptrB = headB;
+
+        while (ptrA != ptrB) {
+            ptrA = ptrA == null ? headB : ptrA.Next;
+            ptrB = ptrB == null ? headA : ptrB.Next;
+        }
+
+        return ptrA;
     }
 }
