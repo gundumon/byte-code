c8788afcf03061db49da113f4ceebffb5cfcbba9
Raju Joseph <raju.joseph@live.com>
2025-10-07 22:27:09 +0530

    ...

--- csharp/coding-patterns/03 Linked Lists/DoublyLinkedListNode.cs
+++ csharp/coding-patterns/03 Linked Lists/DoublyLinkedListNode.cs
@@ -1,4 +1,11 @@
 ﻿namespace CodingPatterns;
 
-public class DoublyLinkedListNode {
+public class DoublyLinkedListNode<T>(int key, T value) {
+    public int Key { get; init; } = key;
+
+    public T Value { get; init; } = value;
+
+    public DoublyLinkedListNode<T>? Next { get; set; }
+
+    public DoublyLinkedListNode<T>? Prev { get; set; }
 }
