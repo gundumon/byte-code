01e50fa5d88dff64bfeda2ec44ee6d4bdc41bc66
Raju Joseph <raju.joseph@live.com>
2025-10-08 22:38:44 +0530

    ...

--- csharp/coding-patterns/03 Linked Lists/LRUCache.cs
+++ csharp/coding-patterns/03 Linked Lists/LRUCache.cs
@@ -5,6 +5,9 @@ Design and implement a data structure for the Least Recently Used (LRU) cache th
 1. LRUCache(capacity: int): Initialize an LRU cache with the specified capacity
 2. get(key: int) -> int: Return the value associated with a key. Return -1 if the key doesn't exist
 3. put(key: int, value: int) -> None: Add a key and its value to the cache. If adding the key would result in the cache exceeding its size capacity, evict the least recently used element. If the key already exists in the cache, update its value
+
+Constraints:
+    1. All keys and values are positive integers
 */
 public class LRUCache {
 
