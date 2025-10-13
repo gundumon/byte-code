eec928f533b05a1ead217353c6c3b1b652819294
Raju Joseph <raju.joseph@live.com>
2025-10-13 18:17:54 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿LinkedListNode<int> a = new(1);
+LinkedListNode<int> b = new(2);
+LinkedListNode<int> c = new(4);
+LinkedListNode<int> d = new(7);
+LinkedListNode<int> e = new(3);
+
+a.Next = b;
+b.Next = c;
+c.Next = d;
+d.Next = e;
