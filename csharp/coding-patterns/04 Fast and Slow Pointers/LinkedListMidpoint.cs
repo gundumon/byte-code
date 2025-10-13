5c2cd5e28f388c1f7b40ca0646d1214ca2b8d944
Raju Joseph <raju.joseph@live.com>
2025-10-13 17:37:10 +0530

    ...

--- csharp/coding-patterns/04 Fast and Slow Pointers/LinkedListMidpoint.cs
+++ csharp/coding-patterns/04 Fast and Slow Pointers/LinkedListMidpoint.cs
@@ -2,6 +2,10 @@
 
 /*
 Given a singly linked list, find and return its middle node. If there are two middle nodes, return the second one.
+
+Constraints:
+    1. The linked list contains at least one node
+    2. The linked list contains unique values
 */
 public class LinkedListMidpoint {
 }
