099082341d864e8adf7cb0e69c5bd7de8930b68c
Raju Joseph <raju.joseph@live.com>
2025-10-14 06:47:53 +0530

    ...

--- csharp/coding-patterns/Program.cs
+++ csharp/coding-patterns/Program.cs
@@ -1,18 +1,1 @@
-﻿using CodingPatterns;
-
-ListNode<int> a = new(1);
-ListNode<int> b = new(2);
-ListNode<int> c = new(4);
-ListNode<int> d = new(7);
-ListNode<int> e = new(3);
-ListNode<int> f = new(5);
-
-a.Next = b;
-b.Next = c;
-c.Next = d;
-d.Next = e;
-e.Next = f;
-
-var midPoint = LinkedListMidpoint.Find(a);
-Console.WriteLine(midPoint?.Value);
-Console.ReadLine();
+﻿
