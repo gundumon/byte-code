4f566f7e2d477fe1abe05f484d4e7e5e83f3acfd
Raju Joseph <raju.joseph@live.com>
2025-10-15 16:39:56 +0530

    ...

--- src/BC.ConsoleApp/Program.cs
+++ src/BC.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿int[] nums = [1, 2, 2, 3, 3, 4, 5];
+﻿using CodingPatterns;
+
+int[] nums = [1, 2, 2, 3, 3, 4, 5];
 int target = 5;
 
-var result = PairSum
+var result = PairSum.FindAnyPair(nums, target);
