d942d2cc1bb5276252d458606bee4ad587803063
Raju Joseph <raju.joseph@live.com>
2025-10-15 16:44:24 +0530

    ...

--- src/BC.ConsoleApp/Program.cs
+++ src/BC.ConsoleApp/Program.cs
@@ -4,3 +4,6 @@ int[] nums = [1, 2, 2, 3, 3, 4, 5];
 int target = 5;
 
 var result = PairSum.FindAnyPair(nums, target);
+Console.WriteLine($"[{string.Join(", ", nums)}] => {target}");
+Console.WriteLine($"[{result?.num1}, {result?.num2}]");
+Console.ReadLine();
