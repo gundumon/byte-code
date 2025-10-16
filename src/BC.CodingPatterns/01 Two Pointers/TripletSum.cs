b9db3f352141e36f23e5f8eab73a51fc95959b53
Raju Joseph <raju.joseph@live.com>
2025-10-16 19:23:02 +0530

    ...

--- src/BC.CodingPatterns/01 Two Pointers/TripletSum.cs
+++ src/BC.CodingPatterns/01 Two Pointers/TripletSum.cs
@@ -2,10 +2,6 @@
 
 /*
 Given an array of integers, return all triplets [a, b, c] such that a + b + c = 0 . The solution must not contain duplicate triplets (e.g., [1, 2, 3] and [2, 3, 1] are considered duplicates). If no such triplets are found, return an empty array.
-
-Example:
-    Input: [0, -1, 2, -3, 1]
-    Output: [[-3, 1, 2], [-1, 0, 1]]
 */
 public class TripletSum {
     public static HashSet<Tuple<int, int, int>> FindAllTriplets(int[] nums) {
