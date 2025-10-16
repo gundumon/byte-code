b3d1f956deea51fa963bd19e1760a85a33a60db1
Raju Joseph <raju.joseph@live.com>
2025-10-16 07:41:43 +0530

    ...

--- src/BC.CodingPatterns/01 Two Pointers/PairSum.cs
+++ src/BC.CodingPatterns/01 Two Pointers/PairSum.cs
@@ -46,24 +46,33 @@ public class PairSum {
             var sum = nums[left] + nums[right];
 
             if (sum == target) {
-                int leftValue = nums[left];
-                int rightValue = nums[right];
+                int leftNum = nums[left];
+                int rightNum = nums[right];
 
-                if (leftValue != rightValue) {
+                if (leftNum == rightNum) {
+                    var count = right - left + 1;
+
+                    for (var i = 0; i < count; i ++) {
+                        for (var j = i + 1; j < count; j++) {
+                            result.Add([left + i, left + j]);
+                        }
+                    }
+                }
+                else {
                     int tempL = left;
 
-                    while (tempL < right && nums[tempL] == leftValue) {
+                    while (tempL < right && nums[tempL] == leftNum) {
                         tempL++;
                     }
-                    
+
                     int countL = tempL - left;
 
                     int tempR = right;
 
-                    while (tempR > left && nums[tempR] == rightValue) {
+                    while (tempR > left && nums[tempR] == rightNum) {
                         tempR--;
                     }
-                    
+
                     int countR = right - tempR;
 
                     for (int i = 0; i < countL; i++) {
@@ -74,17 +83,6 @@ public class PairSum {
 
                     left = tempL;
                     right = tempR;
-                }
-                else {
-                    int N = right - left + 1;
-
-                    for (int i = 0; i < N; i++) {
-                        for (int j = i + 1; j < N; j++) {
-                            result.Add([left + i, left + j]);
-                        }
-                    }
-
-                    left = right;
                 }
             }
             else if (sum < target) {
