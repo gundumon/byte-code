82ab4d8343bd866c426ac07acd50bfc423d495a9
Raju Joseph <raju.joseph@live.com>
2025-10-16 07:49:15 +0530

    ...

--- src/BC.CodingPatterns/01 Two Pointers/PairSum.cs
+++ src/BC.CodingPatterns/01 Two Pointers/PairSum.cs
@@ -14,20 +14,20 @@ public class PairSum {
             return [];
 
         List<int[]> result = [];
-        int left = 0;
-        int right = nums.Length - 1;
+        int leftPtr = 0;
+        int rightPtr = nums.Length - 1;
 
-        while (left < right) {
-            var sum = nums[left] + nums[right];
+        while (leftPtr < rightPtr) {
+            var sum = nums[leftPtr] + nums[rightPtr];
             if (sum == target) {
-                result.Add([left, right]);
+                result.Add([leftPtr, rightPtr]);
                 break;
             }
             else if (sum < target) {
-                left += 1;
+                leftPtr += 1;
             }
             else {
-                right -= 1;
+                rightPtr -= 1;
             }
         }
 
@@ -39,57 +39,57 @@ public class PairSum {
             return [];
 
         List<int[]> result = [];
-        int left = 0;
-        int right = nums.Length - 1;
+        int leftPtr = 0;
+        int rightPtr = nums.Length - 1;
 
-        while (left < right) {
-            var sum = nums[left] + nums[right];
+        while (leftPtr < rightPtr) {
+            var sum = nums[leftPtr] + nums[rightPtr];
 
             if (sum == target) {
-                int leftNum = nums[left];
-                int rightNum = nums[right];
+                int leftNum = nums[leftPtr];
+                int rightNum = nums[rightPtr];
 
                 if (leftNum == rightNum) {
-                    var count = right - left + 1;
+                    var count = rightPtr - leftPtr + 1;
 
                     for (var i = 0; i < count; i ++) {
                         for (var j = i + 1; j < count; j++) {
-                            result.Add([left + i, left + j]);
+                            result.Add([leftPtr + i, leftPtr + j]);
                         }
                     }
                 }
                 else {
-                    int tempL = left;
+                    int tempL = leftPtr;
 
-                    while (tempL < right && nums[tempL] == leftNum) {
+                    while (tempL < rightPtr && nums[tempL] == leftNum) {
                         tempL++;
                     }
 
-                    int countL = tempL - left;
+                    int countL = tempL - leftPtr;
 
-                    int tempR = right;
+                    int tempR = rightPtr;
 
-                    while (tempR > left && nums[tempR] == rightNum) {
+                    while (tempR > leftPtr && nums[tempR] == rightNum) {
                         tempR--;
                     }
 
-                    int countR = right - tempR;
+                    int countR = rightPtr - tempR;
 
                     for (int i = 0; i < countL; i++) {
                         for (int j = 0; j < countR; j++) {
-                            result.Add([left + i, right - j]);
+                            result.Add([leftPtr + i, rightPtr - j]);
                         }
                     }
 
-                    left = tempL;
-                    right = tempR;
+                    leftPtr = tempL;
+                    rightPtr = tempR;
                 }
             }
             else if (sum < target) {
-                left += 1;
+                leftPtr += 1;
             }
             else {
-                right -= 1;
+                rightPtr -= 1;
             }
         }
 
