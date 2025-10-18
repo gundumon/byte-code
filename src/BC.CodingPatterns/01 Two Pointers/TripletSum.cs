24c8e629bce0e75e4e5eb7b89b144ead3a599f06
Raju Joseph <raju.joseph@live.com>
2025-10-18 10:49:41 +0530

    ...

--- src/BC.CodingPatterns/01 Two Pointers/TripletSum.cs
+++ src/BC.CodingPatterns/01 Two Pointers/TripletSum.cs
@@ -4,7 +4,7 @@
 Given an array of integers, return all triplets [a, b, c] such that a + b + c = 0 . The solution must not contain duplicate triplets (e.g., [1, 2, 3] and [2, 3, 1] are considered duplicates). If no such triplets are found, return an empty array.
 */
 public class TripletSum {
-    public static HashSet<Tuple<int, int, int>> FindAllTriplets(int[] nums) {
+    public static HashSet<(int a, int b, int c)> FindAllTriplets(int[] nums) {
         var triplets = new HashSet<Tuple<int, int, int>>();
         Array.Sort(nums);
 
@@ -13,7 +13,9 @@ public class TripletSum {
             var target = nums[targetIndex];
             var startIndex = targetIndex + 1;
             var temp = nums[startIndex..];
+            
             var pairs = PairSum.FindAllPairs(temp, target * -1);
+
             foreach (var pair in pairs)
                 triplets.Add(Tuple.Create(target, pair[0], pair[1]));
 
