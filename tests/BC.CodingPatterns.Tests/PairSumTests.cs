30ca201d2df9c2c6411ed5ae3a7781180d40750f
Raju Joseph <raju.joseph@live.com>
2025-10-22 19:36:19 +0530

    ...

--- tests/BC.CodingPatterns.Tests/PairSumTests.cs
+++ tests/BC.CodingPatterns.Tests/PairSumTests.cs
@@ -1,8 +1,15 @@
-﻿namespace BC.CodingPatterns.Tests;
+﻿using CodingPatterns;
+
+namespace BC.CodingPatterns.Tests;
 
 public class PairSumTests {
     [Fact]
     public void FindAnyPair_EmptyInput_ReturnsNull() {
+        int[] nums = [];
+        int target = 5;
 
+        (int, int)? result = PairSum.FindAnyPair(nums, target);
+
+        Assert.Null(result);
     }
 }
