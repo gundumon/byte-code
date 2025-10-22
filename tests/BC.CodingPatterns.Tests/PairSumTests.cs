c77b24d72ebab836caecaa782576f4a1468f77d2
Raju Joseph <raju.joseph@live.com>
2025-10-22 19:46:54 +0530

    ...

--- tests/BC.CodingPatterns.Tests/PairSumTests.cs
+++ tests/BC.CodingPatterns.Tests/PairSumTests.cs
@@ -22,4 +22,36 @@ public class PairSumTests {
 
         Assert.Null(result);
     }
+
+    [Fact]
+    public void FindAnyPair_InputContainsNoPairs_ReturnsNull() {
+        int[] nums = [1, 2, 5];
+        int target = 5;
+
+        (int, int)? result = PairSum.FindAnyPair(nums, target);
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void FindAnyPair_OneMatchingPair_ReturnsPair() {
+        int[] nums = [1, 2, 3, 5];
+        int target = 5;
+
+        (int a, int b)? result = PairSum.FindAnyPair(nums, target);
+
+        Assert.NotNull(result);
+        Assert.Equal(target, result.Value.a + result.Value.b);
+    }
+
+    [Fact]
+    public void FindAnyPair_MultipleMatchingPairs_ReturnsPair() {
+        int[] nums = [1, 2, 3, 4, 5];
+        int target = 5;
+
+        (int a, int b)? result = PairSum.FindAnyPair(nums, target);
+
+        Assert.NotNull(result);
+        Assert.Equal(target, result.Value.a + result.Value.b);
+    }
 }
