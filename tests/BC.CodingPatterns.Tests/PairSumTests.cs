93c048284aedc9c05b278ad5c963ef75cd8916a6
Raju Joseph <raju.joseph@live.com>
2025-10-25 22:24:05 +0530

    ...

--- tests/BC.CodingPatterns.Tests/PairSumTests.cs
+++ tests/BC.CodingPatterns.Tests/PairSumTests.cs
@@ -90,7 +90,7 @@ public class PairSumTests {
     }
 
     [Theory]
-    [InlineData(new int[] { 1, 2, 3, 4, 5 }, 5, 2)]
+    [InlineData([new int[] { 1, 1, 1 }, 2, 3])]
     public void FindAllPairs_MatchingPairs_ReturnsAllPairs(int[] nums, int target, int expectedCount) {
         List<(int, int)> result = PairSum.FindAllPairs(nums, target);
 
