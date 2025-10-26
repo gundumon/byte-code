920b88a833b855379586bf777fd569d79c673f87
Raju Joseph <raju.joseph@live.com>
2025-10-26 11:44:51 +0530

    ...

--- tests/BC.CodingPatterns.Tests/PairSumTests.cs
+++ tests/BC.CodingPatterns.Tests/PairSumTests.cs
@@ -75,21 +75,23 @@ public class PairSumTests {
         Assert.Empty(result);
     }
 
-    [Fact]
-    public void FindAllPairs_OneMatchingPair_ReturnsPair() {
-        int[] nums = [1, 2, 3, 5];
-        int target = 5;
-
+    [Theory]
+    [InlineData(new int[] { }, 0)]
+    [InlineData(new int[] { 2, 4 }, 5)]
+    public void FindAllPairs_NoMatchingPair_ReturnsEmpty(int[] nums, int target) {
         List<(int, int)> result = PairSum.FindAllPairs(nums, target);
 
         Assert.NotNull(result);
-        Assert.NotEmpty(result);
-        Assert.Single(result);
+        Assert.Empty(result);
     }
 
     [Theory]
-    [InlineData([new int[] { -3, -2, -1 }, -5, 1])]
+    [InlineData([new int[] { 2, 3 }, 5, 1])]
     [InlineData([new int[] { 1, 1, 1 }, 2, 3])]
+    [InlineData([new int[] { 2, 2, 3 }, 5, 2])]
+    [InlineData([new int[] { -1, 2, 3 }, 2, 1])]
+    [InlineData([new int[] { -3, -2, -1 }, -5, 1])]
+    [InlineData([new int[] { 1, 2, 3, 5 }, 5, 1])]
     [InlineData([new int[] { -5, -2, 2, 3, 4, 5, 9 }, 7, 3])]
     public void FindAllPairs_MatchingPairs_ReturnsAllPairs(int[] nums, int target, int expectedCount) {
         List<(int, int)> result = PairSum.FindAllPairs(nums, target);
