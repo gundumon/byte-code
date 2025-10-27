d80fbf17e9eb884340b505ab91bd916c074b051a
Raju Joseph <raju.joseph@live.com>
2025-10-27 20:02:07 +0530

    ...

--- src/BC.CodingPatterns/02-TripletSum.cs
+++ src/BC.CodingPatterns/02-TripletSum.cs
@@ -1,6 +1,4 @@
-﻿using BC.CodingPatterns;
-
-namespace CodingPatterns;
+﻿namespace BC.CodingPatterns;
 
 /*
 Given an array of integers, return all triplets [a, b, c] such that a + b + c = 0 . The solution must not contain duplicate triplets (e.g., [1, 2, 3] and [2, 3, 1] are considered duplicates). If no such triplets are found, return an empty array.
