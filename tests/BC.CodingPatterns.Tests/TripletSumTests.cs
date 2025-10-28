ecaf9cc8688ede90e5955337c4a2abb2f1b1d262
Raju Joseph <raju.joseph@live.com>
2025-10-28 16:58:42 +0530

    ...

--- tests/BC.CodingPatterns.Tests/TripletSumTests.cs
+++ tests/BC.CodingPatterns.Tests/TripletSumTests.cs
@@ -13,7 +13,7 @@ public class TripletSumTests {
 
     [Fact]
     public void FindAllTriplets_InputLengthLessThanThree_ReturnsEmpty() {
-        int[] nums = [];
+        int[] nums = [1, 2];
 
         HashSet<(int a, int b, int c)> result = TripletSum.FindAllTriplets(nums);
 
@@ -22,28 +22,22 @@ public class TripletSumTests {
     }
 
     [Theory]
-    [InlineData(new int[] { }, 0)]
-    [InlineData(new int[] { 2, 4 }, 5)]
-    public void FindAllTriplets_NoMatchingPair_ReturnsEmpty(int[] nums, int target) {
-        List<(int, int)> result = PairSum.FindAllPairs(nums, target);
+    [InlineData(new int[] { -2, 0, 3})]
+    [InlineData(new int[] { -3, -2, 1, 4 })]
+    public void FindAllTriplets_NoMatchingPair_ReturnsEmpty(int[] nums) {
+        HashSet<(int a, int b, int c)> result = TripletSum.FindAllTriplets(nums);
 
         Assert.NotNull(result);
         Assert.Empty(result);
     }
 
     [Theory]
-    [InlineData([new int[] { 2, 3 }, 5, 1])]
-    [InlineData([new int[] { 1, 1, 1 }, 2, 3])]
-    [InlineData([new int[] { 2, 2, 3 }, 5, 2])]
-    [InlineData([new int[] { -1, 2, 3 }, 2, 1])]
-    [InlineData([new int[] { -3, -2, -1 }, -5, 1])]
-    [InlineData([new int[] { 1, 2, 3, 5 }, 5, 1])]
-    [InlineData([new int[] { -5, -2, 2, 3, 4, 5, 9 }, 7, 3])]
-    public void FindAllTriplets_MatchingPairs_ReturnsAllPairs(int[] nums, int target, int expectedCount) {
-        List<(int, int)> result = PairSum.FindAllPairs(nums, target);
+    [InlineData([new int[] { -3, -1, 0, 1, 2 }, 2])]
+    public void FindAllTriplets_MatchingPairs_ReturnsAllPairs(int[] nums, int expectedCount) {
+        HashSet<(int a, int b, int c)> result = TripletSum.FindAllTriplets(nums);
 
         Assert.NotEmpty(result);
         Assert.Equal(expectedCount, result.Count);
-        Assert.All(result, (x) => target.Equals(x.Item1 + x.Item2));
+        Assert.All(result, (x) => (x.a + x.b + x.c).Equals(0));
     }
 }
