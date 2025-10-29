81f6380de866b289645c197d5df7849904e5b25d
Raju Joseph <raju.joseph@live.com>
2025-10-29 21:29:24 +0530

    ...

--- src/BC.CodingPatterns/03-Palindrome.cs
+++ src/BC.CodingPatterns/03-Palindrome.cs
@@ -1,4 +1,4 @@
-namespace CodingPatterns;
+namespace BC.CodingPatterns;
 
 /*
 Given a string, determine if it's a palindrome after removing all non-alphanumeric characters. A character is alphanumeric if it's either a letter or a number.
