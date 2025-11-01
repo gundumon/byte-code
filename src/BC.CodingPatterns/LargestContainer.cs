46db1b631acb1086613dff5389d25f558f8aa34a
Raju Joseph <raju.joseph@live.com>
2025-11-01 21:41:37 +0530

    ...

--- src/BC.CodingPatterns/LargestContainer.cs
+++ src/BC.CodingPatterns/LargestContainer.cs
@@ -1,4 +1,4 @@
-﻿namespace CodingPatterns;
+﻿namespace BC.CodingPatterns;
 
 /*
 You are given an array of numbers, each representing the height of a vertical line on a graph. A container can be formed with any pair of these lines, along with the x-axis of the graph. Return the amount of water which the largest container can hold.
