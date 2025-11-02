429595a5469bd72946a77dc67ecf2a7ec9371bb4
Raju Joseph <raju.joseph@live.com>
2025-11-02 14:27:59 +0530

    ...

--- tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
+++ tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
@@ -1,6 +1,11 @@
 ﻿namespace BC.CodingPatterns.Tests;
 
 public class LargestContainerTests {
-    [Fact]
+    [Theory]
+    [InlineData([new int[] { }])]
+    public void Find_InvalidLength_ReturnsZero(int[] heights) {
+        var result = LargestContainer.Find(heights);
 
+        Assert.Equal(0, result);
+    }
 }
