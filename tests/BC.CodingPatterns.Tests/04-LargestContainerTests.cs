6d19a843c11c9b61ce4a9b3b3a90d1400f3c0143
Raju Joseph <raju.joseph@live.com>
2025-11-02 14:16:27 +0530

    ...

--- tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
+++ tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace BC.CodingPatterns.Tests;
 
-namespace BC.CodingPatterns.Tests {
-    internal class LargestContainerTests {
-    }
+public class LargestContainerTests {
 }
