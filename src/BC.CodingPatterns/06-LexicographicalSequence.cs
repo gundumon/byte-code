e24d09d842c3df47540538830c79df6ccc34d15b
Raju Joseph <raju.joseph@live.com>
2025-11-06 07:46:02 +0530

    ...

--- src/BC.CodingPatterns/06-LexicographicalSequence.cs
+++ src/BC.CodingPatterns/06-LexicographicalSequence.cs
@@ -3,6 +3,14 @@
 public class LexicographicalSequence {
     /*
     Given a string of lowercase English letters, rearrange the characters to form a new string representing the next immediate sequence in lexicographical (alphabetical) order. If the given string is already last in lexicographical order among all possible arrangements, return the arrangement that's first in lexicographical order.
+
+    Logic:
+      1. Locate the pivot
+        - Pivot is the first character that breaks the non-incrceasing sequence from right of the string
+        - If no pivot is found, the string is already at its last lexicographical sequence, and the result is just the reverse of the string
+      2. Find the rightmost successor to the pivot
+      3. Swap the rightmost successor with the pivot to increase the lexicographical order of the suffix
+      4. Reverse the suffix after the pivot to minimize its permutation
     */
     public static string NextSequence(string input) {
         var letters = input.ToArray();
