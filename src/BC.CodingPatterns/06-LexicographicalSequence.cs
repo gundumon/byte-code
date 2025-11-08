bb7370c9e0feb5d4b378ec6e30cc2ae635ea9e0d
Raju Joseph <raju.joseph@live.com>
2025-11-08 22:58:33 +0530

    ...

--- src/BC.CodingPatterns/06-LexicographicalSequence.cs
+++ src/BC.CodingPatterns/06-LexicographicalSequence.cs
@@ -6,7 +6,7 @@ public class LexicographicalSequence {
 
     Logic:
       1. Locate the pivot
-        - Pivot is the first character that breaks the incrceasing sequence from right of the string
+        - Pivot is the first character from the right that breaks the increasing sequence
         - If no pivot is found, the string is already at its last lexicographical sequence, and the result is just the reverse of the string
       2. Find the rightmost successor (character larger than the pivot)
       3. Swap the rightmost successor with the pivot to increase the lexicographical order of the suffix
