28f743966d3ee47eaff8c4da92bc6d3a76f4a59b
Raju Joseph <raju.joseph@live.com>
2025-11-09 21:38:32 +0530

    ...

--- src/BC.CodingPatterns/07-PairSumUnsorted.cs
+++ src/BC.CodingPatterns/07-PairSumUnsorted.cs
@@ -1,13 +1,9 @@
-﻿namespace CodingPatterns;
+﻿namespace BC.CodingPatterns;
 
 /*
 Given an array of integers, return the indexes of any two numbers that add up to a target. The order of the indexes in the result doesn't matter. If no pair is found, return an empty array.
 
 Constraint: The same index cannot be used twice in the result
-
-Example:
-    Input: nums = [-1, 3, 4, 2], target = 3
-    Output: [0, 2]
 */
 public class PairSumUnsorted {
     public static int[] FindPairs(int[] nums, int target) {
