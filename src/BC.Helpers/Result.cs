8fa2144e3e1dde503041190720509c6ec0b31064
Raju Joseph <raju.joseph@live.com>
2025-11-10 08:13:44 +0530

    ...

--- src/BC.Helpers/Result.cs
+++ src/BC.Helpers/Result.cs
@@ -1,14 +1,36 @@
 ﻿namespace BC.Helpers;
 
 public class Result {
-    protected Result(bool isSuccess, Error? error) {
+    protected Result(bool isSuccess, Error error) {
         IsSuccess = isSuccess;
         Error = error;
     }
 
     public bool IsSuccess { get; }
 
-    public Error? Error { get; }
+    public Error Error { get; }
 
-    public static Result Success() => new(true, null);
+    public static Result Success() => new(true, Error.Empty());
+
+    public static Result Failure(Error error) => new(false, error);
+}
+
+public class Result<T> : Result {
+    private readonly T? _value;
+
+    private Result(T value) : base(true, Error.Empty()) {
+        _value = value;
+    }
+
+    private Result(Error error) : base(false, error) {
+        _value = default;
+    }
+
+    public T? Value {
+        get => IsSuccess ? _value! : throw new InvalidOperationException("Cannot access value when IsSuccess is false.");
+    }
+
+    public static Result<T> Success(T value) => new(value);
+
+    public static Result<T> Failure(Error error) => new(error);
 }
