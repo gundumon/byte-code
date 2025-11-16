0e3dcbfebb6d1b94c6b09b44e231be1b401d4cc2
Raju Joseph <raju.joseph@live.com>
2025-11-16 18:26:30 +0530

    ...

--- tests/BC.CodingPatterns.Tests/08-SudokuBoardTests.cs
+++ tests/BC.CodingPatterns.Tests/08-SudokuBoardTests.cs
@@ -3,7 +3,21 @@
 public class SudokuBoardTests {
     [Fact]
     public void Verify_ValidBoard_ReturnsTrue() {
-        
+        List<List<int>> board = [
+            [3, 0, 6, 0, 5, 8, 4, 0, 0],
+            [5, 2, 0, 0, 0, 0, 0, 0, 0],
+            [0, 8, 7, 0, 0, 0, 0, 3, 1],
+            [1, 0, 2, 5, 0, 0, 3, 0, 0],
+            [9, 0, 0, 8, 6, 3, 0, 0, 5],
+            [0, 5, 0, 0, 9, 0, 6, 0, 0],
+            [0, 3, 0, 0, 0, 0, 2, 5, 0],
+            [0, 1, 0, 0, 0, 0, 0, 7, 4],
+            [0, 0, 5, 2, 0, 6, 0, 0, 0]
+        ];
+
+        var result = SudokuBoard.Verify(board);
+
+        Assert.True(result);
     }
 
     [Fact]
