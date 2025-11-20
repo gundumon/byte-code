c5b3d282fdd5b2156f910c7c75a84bd1d883d7a3
Raju Joseph <raju.joseph@live.com>
2025-11-20 19:35:26 +0530

    ...

--- src/BC.CodingPatterns/03-Palindrome.cs
+++ src/BC.CodingPatterns/03-Palindrome.cs
@@ -11,5 +11,24 @@ public class Palindrome {
 
         int left = 0;
         int right = input.Length - 1;
+
+        while (left < right) {
+            while (left < right && !char.IsLetterOrDigit(input[left])) {
+                left += 1;
+            }
+
+            while (left < right && !char.IsLetterOrDigit(input[right])) {
+                right -= 1;
+            }
+
+            if (left < right && char.ToLower(input[left]) != char.ToLower(input[right])) {
+                return false;
+            }
+
+            left += 1;
+            right -= 1;
+        }
+
+        return true;
     }
 }
