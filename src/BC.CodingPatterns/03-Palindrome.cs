08ac67eccabc227a0abde186440e7becf5e54b64
Raju Joseph <raju.joseph@live.com>
2025-11-20 19:29:18 +0530

    ...

--- src/BC.CodingPatterns/03-Palindrome.cs
+++ src/BC.CodingPatterns/03-Palindrome.cs
@@ -5,29 +5,6 @@ Given a string, determine if it's a palindrome after removing all non-alphanumer
 */
 public class Palindrome {
     public static bool IsValid(string input) {
-        if (string.IsNullOrEmpty(input)) {
-            return true;
-        }
-
-        var left = 0;
-        var right = input.Length - 1;
-
-        while (left < right) {
-            while ((left < right) && !char.IsLetterOrDigit(input[left])) {
-                left += 1;
-            }
-
-            while ((left < right) && !char.IsLetterOrDigit(input[right])) {
-                right -= 1;
-            }
-
-            if (input[left] != input[right])
-                return false;
-
-            left += 1;
-            right -= 1;
-        }
-
-        return true;
+        
     }
 }
