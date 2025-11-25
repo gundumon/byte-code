c12a2947c9cb96295a5de68154802c8099868f27
Raju Joseph <raju.joseph@live.com>
2025-11-25 19:17:02 +0530

    ...

--- tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
+++ tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
@@ -13,6 +13,17 @@ public class LargestContainerTests {
         while (left < right) {
             var area = Math.Min(heights[left], heights[right]) * (right - left);
             largest_area = Math.Max(largest_area, area);
+
+            if (heights[left] == heights[right]) {
+                left += 1;
+                right -= 1;
+            }
+            else if (heights[left] < heights[right]) {
+                left += 1;
+            }
+            else {
+                right -= 1;
+            }
         }
 
         return largest_area;
