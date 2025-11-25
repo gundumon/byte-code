6332b7a8e9188de94d8bf23e33c126d50d0191b3
Raju Joseph <raju.joseph@live.com>
2025-11-25 19:19:20 +0530

    ...

--- src/BC.CodingPatterns/04-LargestContainer.cs
+++ src/BC.CodingPatterns/04-LargestContainer.cs
@@ -4,5 +4,31 @@
 You are given an array of numbers, each representing the height of a vertical line on a graph. A container can be formed with any pair of these lines, along with the x-axis of the graph. Return the amount of water which the largest container can hold.
 */
 public class LargestContainer {
-    
+    public static int FindLargestArea(int[] heights) {
+        if (heights.Length < 2) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = heights.Length - 1;
+        int largest_area = 0;
+
+        while (left < right) {
+            var area = Math.Min(heights[left], heights[right]) * (right - left);
+            largest_area = Math.Max(largest_area, area);
+
+            if (heights[left] == heights[right]) {
+                left += 1;
+                right -= 1;
+            }
+            else if (heights[left] < heights[right]) {
+                left += 1;
+            }
+            else {
+                right -= 1;
+            }
+        }
+
+        return largest_area;
+    }
 }
--- tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
+++ tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
@@ -1,31 +1,12 @@
 ﻿namespace BC.CodingPatterns.Tests;
 
 public class LargestContainerTests {
-    public static int FindLargestArea(int[] heights) {
-        if (heights.Length < 2) {
-            return 0;
-        }
+    [Fact]
+    public void FindLargestArea__EmptyInput_ReturnsZero() {
+        int[] heights = [];
 
-        int left = 0;
-        int right = heights.Length - 1;
-        int largest_area = 0;
+        var result = LargestContainer.FindLargestArea(heights);
 
-        while (left < right) {
-            var area = Math.Min(heights[left], heights[right]) * (right - left);
-            largest_area = Math.Max(largest_area, area);
-
-            if (heights[left] == heights[right]) {
-                left += 1;
-                right -= 1;
-            }
-            else if (heights[left] < heights[right]) {
-                left += 1;
-            }
-            else {
-                right -= 1;
-            }
-        }
-
-        return largest_area;
+        Assert.Equal(0, result);
     }
 }
