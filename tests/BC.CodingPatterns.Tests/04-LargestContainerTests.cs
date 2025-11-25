616ab27496ae7534a88e674d203c7cc9480cc250
Raju Joseph <raju.joseph@live.com>
2025-11-25 19:23:02 +0530

    ...

--- tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
+++ tests/BC.CodingPatterns.Tests/04-LargestContainerTests.cs
@@ -21,6 +21,10 @@ public class LargestContainerTests {
 
     [Theory]
     [InlineData(new int[] { 0, 1, 0 }, 0)]
+    [InlineData(new int[] { 3, 3, 3, 3 }, 9)]
+    [InlineData(new int[] { 1, 2, 3 }, 2)]
+    [InlineData(new int[] { 3, 2, 1 }, 2)]
+    [InlineData(new int[] { 2, 7, 8, 3, 7, 6 }, 24)]
     public void FindLargestArea_ValidInput_ReturnsZero(int[] heights, int expected) {
         var result = LargestContainer.FindLargestArea(heights);
 
