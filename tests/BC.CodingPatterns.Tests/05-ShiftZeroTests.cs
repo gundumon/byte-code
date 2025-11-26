c9325caa9c5d348d6a2aabbafa6004b9564e7f06
Raju Joseph <raju.joseph@live.com>
2025-11-26 19:59:42 +0530

    ...

--- tests/BC.CodingPatterns.Tests/05-ShiftZeroTests.cs
+++ tests/BC.CodingPatterns.Tests/05-ShiftZeroTests.cs
@@ -1,25 +1,5 @@
 ﻿namespace BC.CodingPatterns.Tests;
 
 public class ShiftZeroTests {
-    [Fact]
-    public void ShiftToEnd_EmptyInput_ReturnsEmpty() {
-        int[] nums = [];
-
-        var result = ShiftZero.ShiftToEnd(nums);
-
-        Assert.Empty(result);
-    }
-
-    [Theory]
-    [InlineData([new int[] { 0 }, new int[] { 0 }])]
-    [InlineData([new int[] { 1 }, new int[] { 1 }])]
-    [InlineData([new int[] { 0, 0, 0 }, new int[] { 0, 0, 0 }])]
-    [InlineData([new int[] { 1, 3, 2 }, new int[] { 1, 3, 2 }])]
-    [InlineData([new int[] { 1, 1, 1, 0, 0 }, new int[] { 1, 1, 1, 0, 0 }])]
-    [InlineData([new int[] { 0, 0, 1, 1, 1 }, new int[] { 1, 1, 1, 0, 0 }])]
-    public void ShiftToEnd_ValidInput_ShiftsZerosToEnd(int[] nums, int[] expectedResult) {
-        var result = ShiftZero.ShiftToEnd(nums);
-
-        Assert.Equal(expectedResult, result);
-    }
+    //
 }
