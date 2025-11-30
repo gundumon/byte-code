00df333adac86d4e157125778007c3b2897fa50a
Raju Joseph <raju.joseph@live.com>
2025-11-30 20:10:00 +0530

    ...

--- tests/BC.CodingPatterns.Tests/05-ShiftZeroesTests.cs
+++ tests/BC.CodingPatterns.Tests/05-ShiftZeroesTests.cs
@@ -12,6 +12,10 @@ public class ShiftZeroesTests {
 
     [Fact]
     public void ShiftToEnd_InputLengthLessThanTwo_ReturnsInput() {
+        int[] nums = [0];
 
+        var result = ShiftZeroes.ShiftToEnd(nums);
+
+        Assert.True(Enumerable.SequenceEqual(nums, result));
     }
 }
