cff5a375a9548ddb34144437478bf20bdd3df256
Raju Joseph <raju.joseph@live.com>
2025-12-01 20:45:50 +0530

    ...

--- src/BC.CodingPatterns/06-LexicographicalSequence.cs
+++ src/BC.CodingPatterns/06-LexicographicalSequence.cs
@@ -9,24 +9,29 @@ public class LexicographicalOrder {
             return string.Empty;
         }
 
-        var letters = input.ToArray();
+        var letters = input.ToCharArray();
         var pivot = input.Length - 2;
 
+        // find the first character from right (pivot) that breaks the increasing sequence
         while ((pivot >= 0) && letters[pivot] >= letters[pivot + 1]) {
             pivot -= 1;
         }
 
+        // if not found, string is already in the last lexicographical sequence. so just reverse the original string and return
         if (pivot < 0) {
             return string.Join("", letters.Reverse());
         }
 
+        // find the first character (successor) from the right which is larger than the pivot character
         var successor = letters.Length - 1;
         while (letters[successor] <= letters[pivot]) {
             successor -= 1;
         }
 
+        // swap the characters at pivot and successor
         (letters[pivot], letters[successor]) = (letters[successor], letters[pivot]);
 
+        // reverse the string after the pivot
         var temp = pivot + 1;
         letters.AsSpan(temp).Reverse();
 
