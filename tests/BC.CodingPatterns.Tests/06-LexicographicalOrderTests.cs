f1de7422505f5835380694d5a136d20afdfab0fb
Raju Joseph <raju.joseph@live.com>
2025-12-01 20:48:45 +0530

    ...

---
+++ tests/BC.CodingPatterns.Tests/06-LexicographicalOrderTests.cs
@@ -0,0 +1,25 @@
+﻿namespace BC.CodingPatterns.Tests;
+
+public class LexicographicalOrderTests {
+    [Fact]
+    public void NextSequence_EmptyInput_ReturnsEmptyString() {
+        var input = "";
+
+        var result = LexicographicalOrder.NextSequence(input);
+
+        Assert.True(result == string.Empty);
+    }
+
+    [Theory]
+    [InlineData(["a", "a"])]
+    [InlineData(["abcd", "abdc"])]
+    [InlineData(["dcba", "abcd"])]
+    [InlineData(["aaaa", "aaaa"])]
+    [InlineData(["ynitsed", "ynsdeit"])]
+    [InlineData(["abcedda", "abdacde"])]
+    public void NextSequence_ValidInput_ReturnsNextLexicographicalSequence(string input, string expected) {
+        var result = LexicographicalOrder.NextSequence(input);
+
+        Assert.Equal(expected, result);
+    }
+}
