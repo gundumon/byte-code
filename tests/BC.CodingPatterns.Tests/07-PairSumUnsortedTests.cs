471c881e565f9d801e0d0513ed199c13733176ee
Raju Joseph <raju.joseph@live.com>
2025-12-03 19:01:54 +0530

    ...

--- tests/BC.CodingPatterns.Tests/07-PairSumUnsortedTests.cs
+++ tests/BC.CodingPatterns.Tests/07-PairSumUnsortedTests.cs
@@ -1,5 +1,13 @@
 ﻿namespace BC.CodingPatterns.Tests;
 
 public class PairSumUnsortedTests {
-    
+    [Fact]
+    public void FindIndex_EmptyInput_ReturnsFailure() {
+        int[] nums = [];
+        int target = 5;
+
+        var result = PairSumUnsorted.FindIndex(nums, target);
+
+        Assert.False(result.IsSuccess);
+    }
 }
