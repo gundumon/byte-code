41feb1d18f532c478b842f70b82ce3898e5e7afb
Raju Joseph <raju.joseph@live.com>
2025-12-03 18:40:28 +0530

    ...

--- tests/BC.CodingPatterns.Tests/07-PairSumUnsortedTests.cs
+++ tests/BC.CodingPatterns.Tests/07-PairSumUnsortedTests.cs
@@ -1,32 +1,5 @@
 ﻿namespace BC.CodingPatterns.Tests;
 
 public class PairSumUnsortedTests {
-    [Fact]
-    public void FindPairs_InvalidInput_ReturnsNull() {
-        int[] nums = [];
-        int target = 4;
-
-        var result = PairSumUnsorted.FindPairs(nums, target);
-
-        Assert.Null(result);
-    }
-
-    [Fact]
-    public void FindPairs_InputLengthLessThanTwo_ReturnsNull() {
-        int[] nums = [1];
-        int target = 3;
-
-        var result = PairSumUnsorted.FindPairs(nums, target);
-
-        Assert.Null(result);
-    }
-
-    [Theory]
-    [InlineData([new int[] { -1, 3, 4, 2 }, 3])]
-    public void FindPairs_PairFound_ReturnsPair(int[] nums, int target) {
-        var pair = PairSumUnsorted.FindPairs(nums, target);
-
-        Assert.NotNull(pair);
-        Assert.Equal(target, nums[pair.Value.a] + nums[pair.Value.b]);
-    }
+    
 }
