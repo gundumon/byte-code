cd1daf91184bfd35525c3310059169bf89d63e6b
Raju Joseph <raju.joseph@live.com>
2026-01-01 15:37:10 +0530

    ...

--- src/BC.CodingPatterns/Triplet.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BC.CodingPatterns;
-
-public readonly struct Triplet<T>(T x, T y, T z) {
-    public T X { get; init; } = x;
-
-    public T Y { get; init; } = y;
-
-    public T Z { get; init; } = z;
-}
